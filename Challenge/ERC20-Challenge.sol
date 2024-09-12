b2f3633238b4cde6ac9e2c4e66edb1d29d364988
d4rm5 <dantemartinez06@gmail.com>
2024-09-12 18:37:19 -0300

    ERC-20 Implementation

--- Challenge/ERC20-Challenge.sol
+++ Challenge/ERC20-Challenge.sol
@@ -9,35 +9,51 @@ contract MyToken is ERC20 {
 
     // Constructor that mints the initial supply to the deployer of the contract
     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        // Mint the initial supply of tokens to the deployer's address
+        _mint(msg.sender, initialSupply);
     }
 
     // Function to mint new tokens to a specified address
     function mint(address to, uint256 amount) public {
-        // Implement the mint function using the _mint internal function
+        _mint(to, amount);
     }
 
     // Function to burn tokens from a specified address
     function burn(address from, uint256 amount) public {
-        // Implement the burn function using the _burn internal function
+        _burn(from, amount);
     }
 
     // Function to transfer tokens from the caller's address to a specified address
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        // Implement the transfer function using the _transfer internal function
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool isSuccess) {
+        _transfer(msg.sender, to, amount);
+        return isSuccess;
     }
 
     // Function to approve an address to spend a certain amount of tokens on behalf of the caller
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        // Implement the approve function using the _approve internal function
+    function approve(
+        address spender,
+        uint256 amount
+    ) public override returns (bool isSuccess) {
+        _approve(msg.sender, spender, amount);
+        return isSuccess;
     }
 
     // Function to transfer tokens from one address to another using an allowance
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        // Implement the transferFrom function using the _transfer and _approve internal functions
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool isSuccess) {
+        _approve(from, to, amount);
+        _transfer(from, to, amount);
+        return isSuccess;
     }
 
-    function getBalanceOf(address account) public view returns (uint256) {
-        // Implement the getBalanceOf function
+    function getBalanceOf(
+        address account
+    ) public view returns (uint256 balance) {
+        return (balanceOf(account));
     }
 }
